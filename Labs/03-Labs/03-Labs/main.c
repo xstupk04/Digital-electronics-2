a5810ed979ca40370e57077ca8ac8af5ea53fa90
xstupk04 <60606149+xstupk04@users.noreply.github.com>
2020-10-11 11:23:48 +0200

    Labs-03

---
+++ Labs/03-Labs/03-Labs/main.c
@@ -0,0 +1,18 @@
+/*
+ * 03-Labs.c
+ *
+ * Created: 11.10.2020 11:20:55
+ * Author : Mila
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
