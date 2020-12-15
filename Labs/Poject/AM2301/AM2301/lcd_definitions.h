c7213e2482c5cdf930d406a9993e95e041beaf47
xstupk04 <60606149+xstupk04@users.noreply.github.com>
2020-12-15 15:00:20 +0100

    Project

---
+++ Labs/Poject/AM2301/AM2301/lcd_definitions.h
@@ -0,0 +1,16 @@
+#ifndef LCD_DEFINITIONS_H
+#define LCD_DEFINITIONS_H
+
+/* Arduino Uno LCD Keypad Shield */
+#define LCD_PORT        PORTB
+#define LCD_DATA0_PIN   PB4
+#define LCD_DATA1_PIN   PB5
+#define LCD_DATA2_PIN   PB6
+#define LCD_DATA3_PIN   PB7
+#define LCD_RS_PORT     PORTB
+#define LCD_RS_PIN      PB0
+#define LCD_E_PORT      PORTB
+#define LCD_E_PIN       PB1
+/* R/W pin is connected to GND */
+
+#endif // ifndef LCD_DEFINITIONS_H
